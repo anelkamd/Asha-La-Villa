9ecf5f7c3d2d13513de2af815f453ad86aa2b649
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-05-23 23:05:20 +0100

    add file data

---
+++ data.js
@@ -0,0 +1,54 @@
+export const mesClasse = [
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-4",
+    "variant-1",
+    "variant-2",
+    "variant-3"
+]
+
+export const previews = [
+    {
+        Img: "./image/1.jpg",
+        title: "Acceuille",
+        tags: "Votre hotel de luxe de goma...",
+        description: "Soyez le bienvenue dans notre hotel, une belle vue a cote du lac"
+    },
+    {
+        Img: "./image/2.jpg",
+        title: "Restauran",
+        tags: "Votre hotel de luxe de goma...",
+        description: "Soyez le bienvenue dans notre hotel, une belle vue a cote du lac"
+    },
+    {
+        Img: "./image/3.jpg",
+        title: "Logement",
+        tags: "Votre hotel de luxe de goma...",
+        description: "Soyez le bienvenue dans notre hotel, une belle vue a cote du lac"
+    },
+    {
+        Img: "./image/4.jpg",
+        title: "Spa",
+        tags: "Votre hotel de luxe de goma...",
+        description: "Soyez le bienvenue dans notre hotel, une belle vue a cote du lac"
+    },
+    {
+        Img: "./image/5.jpg",
+        title: "Acceuille",
+        tags: "Votre hotel de luxe de goma...",
+        description: "Soyez le bienvenue dans notre hotel, une belle vue a cote du lac"
+    },
+    {
+        Img: "./image/6.jpg",
+        title: "Acceuille",
+        tags: "Votre hotel de luxe de goma...",
+        description: "Soyez le bienvenue dans notre hotel, une belle vue a cote du lac"
+    },
+    {
+        Img: "./image/7.jpg",
+        title: "Acceuille",
+        tags: "Votre hotel de luxe de goma...",
+        description: "Soyez le bienvenue dans notre hotel, une belle vue a cote du lac"
+    },
+]
